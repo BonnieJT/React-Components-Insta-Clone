efb633da586b9ff120625285586532a0b0a8db1d
Bonnie Turnbeaugh <turnbeaugh@comcast.net>
2020-02-28 18:12:32 -0800

    minor cleanups of excess Lines

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -2,7 +2,6 @@
 // console.log your props and see how to add likes to this component so that it will update when you click the icon.
 // HINT: You will need to add an onClick event pass through your props and update the hard coded Likes
 import React from "react";
-import CommentInput from "../CommentSection/CommentInput";
 
 const LikeSection = props => {
   return (
